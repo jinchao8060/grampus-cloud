036831bc9b629ab1a5ec01ba6f31c2c133b1889c
Beck <297513931@qq.com>
2021-06-15 16:35:28 +0800

    :wrench: 新增sentinel相关配置.

---
+++ grampus-service/grampus-service-auth/src/main/java/com/vdegree/grampus/auth/GrampusAuthApplication.java
@@ -0,0 +1,24 @@
+package com.vdegree.grampus.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
+
+/**
+ * Grampus 认证中心
+ *
+ * @author Beck
+ * @since 2021-06-15
+ */
+@SpringBootApplication
+public class GrampusAuthApplication {
+
+    public static void main(String[] args) {
+        System.setProperty("csp.sentinel.app.type", "1");
+        System.setProperty("user.timezone", "UTC");
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(GrampusAuthApplication.class, args);
+    }
+
+}
