fe909799721afa79f21bd8ab93c5f0f673f8ea14
Beck <297513931@qq.com>
2021-07-01 18:38:34 +0800

    :sparkles: 提供灰度发布版本调试接口.

--- grampus-service/grampus-service-admin/src/main/java/com/vdegree/grampus/admin/modules/system/controller/AdminTestController.java
+++ grampus-service/grampus-service-admin/src/main/java/com/vdegree/grampus/admin/modules/system/controller/AdminTestController.java
@@ -2,8 +2,10 @@ package com.vdegree.grampus.admin.modules.system.controller;
 
 import com.vdegree.grampus.auth.modules.system.client.feign.RemoteAuthTestClient;
 import com.vdegree.grampus.common.core.result.Result;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,17 +17,25 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-06-16
  */
 @Slf4j
-@AllArgsConstructor
 @RestController
 @RequestMapping("/admin/test")
 public class AdminTestController {
-	
-	private final RemoteAuthTestClient remoteAuthTestClient;
+
+	@Autowired
+	private RemoteAuthTestClient remoteAuthTestClient;
+
+	@Value("${spring.cloud.nacos.discovery.metadata.gray.version}")
+	private String version;
 
 	@PostMapping("/demo")
 	public Result<String> demo() {
 		String text = remoteAuthTestClient.demo().getData();
 		log.debug("text:{}", text);
 		return Result.success(text);
+	}
+
+	@GetMapping("/version")
+	public Result<String> version() {
+		return Result.success(version);
 	}
 }
