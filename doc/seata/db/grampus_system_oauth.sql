6781e1e605d9a032add0fc13c6cb4466e898cd94
Beck <297513931@qq.com>
2021-08-05 14:04:01 +0800

    :memo: 添加SpringSecurityOAuth2相关表.

---
+++ doc/seata/db/grampus_system_oauth.sql
@@ -0,0 +1,20 @@
+CREATE TABLE sys_oauth_client_details
+(
+    client_id               VARCHAR(256) PRIMARY KEY,
+    resource_ids            VARCHAR(256),
+    client_secret           VARCHAR(256),
+    scope                   VARCHAR(256),
+    authorized_grant_types  VARCHAR(256),
+    web_server_redirect_uri VARCHAR(256),
+    authorities             VARCHAR(256),
+    access_token_validity   INT(11),
+    refresh_token_validity  INT(11),
+    additional_information  VARCHAR(4096),
+    autoapprove             VARCHAR(256)
+);
+
+create table sys_oauth_code
+(
+    code           VARCHAR(256),
+    authentication BLOB
+);
