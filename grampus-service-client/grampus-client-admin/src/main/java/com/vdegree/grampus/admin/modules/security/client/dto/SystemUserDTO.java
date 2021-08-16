45e712723189db7b4e53477618b0f2f9a425592c
Beck <297513931@qq.com>
2021-08-16 11:10:01 +0800

    :recycle: 调整enabled字段为status字段.

--- grampus-service-client/grampus-client-admin/src/main/java/com/vdegree/grampus/admin/modules/security/client/dto/SystemUserDTO.java
+++ grampus-service-client/grampus-client-admin/src/main/java/com/vdegree/grampus/admin/modules/security/client/dto/SystemUserDTO.java
@@ -45,7 +45,7 @@ public class SystemUserDTO implements Serializable {
 	/**
 	 * 是否启用(0停用 1启用)
 	 */
-	private Integer enabled;
+	private Integer status;
 	/**
 	 * 创建者
 	 */
--- grampus-service-client/grampus-client-admin/src/main/java/com/vdegree/grampus/admin/modules/security/client/dto/SystemUserDetailsDTO.java
+++ grampus-service-client/grampus-client-admin/src/main/java/com/vdegree/grampus/admin/modules/security/client/dto/SystemUserDetailsDTO.java
@@ -58,7 +58,7 @@ public class SystemUserDetailsDTO implements Serializable {
 	/**
 	 * 是否启用(0停用 1启用)
 	 */
-	private Integer enabled;
+	private Integer status;
 
 	/**
 	 * 拥有权限
